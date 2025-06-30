c66f9cd83b058bab4f938a9b5c3e13c169c6ea0c
Pinguicidgon <rcidgon@gmail.com>
2025-06-30 10:26:27 +0200

    Primer commit del proyecto ArquitExt

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from "@apollo/server";
+import { typeDefs } from "./schema.ts";
+import { MongoClient } from "mongodb";
+import { RestaurantModel } from "./types.ts";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers } from "./resolvers.ts";
+import "https://deno.land/x/dotenv/load.ts";
+
+const MONGO_URL = Deno.env.get("MONGO_URL");
+
+if (!MONGO_URL) {
+  throw new Error("MONGO_URL is not defined");
+}
+
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+
+console.info("Connected to MongoDB");
+
+const mongoDB = mongoClient.db("ExamenExtraordinario2425");
+const ResturantsCollection =
+  mongoDB.collection<RestaurantModel>("restaurantes");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  context: async () => ({ ResturantsCollection }),
+});
+
+console.info(`Server ready at ${url}`);
